76285d11c5b2ea4ae3aa287348c2b723fa34881f
Osay Gonzalez <osay@mitzi.info>
2025-12-11 12:35:21 -0400

    menos

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/curso-linux.github.io/',
+export default defineConfig(({ mode }) => ({  
   server: {
     host: "::",
     port: 8080,
